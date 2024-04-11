a4244e21b2890004552daed6e410a87ad775d4f1
Pavel Skorynin <pavel.skorynin@gmail.com>
2024-04-11 11:17:50 +0500

    Добавлен тест с П-регулятором на MicroPython

--- MicroPython/performance_test_pybricks_ev3.py
+++ MicroPython/performance_test_pybricks_ev3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (ColorSensor)
+from pybricks.ev3devices import ColorSensor, Motor
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -14,7 +14,7 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 timer = StopWatch()
-s = ColorSensor(Port.S1)
+s1 = ColorSensor(Port.S1)
 
 ev3.screen.print('Performance test MicroPython')
 print("Performance test MicroPython")
@@ -35,7 +35,7 @@ count = 0
 sum = 0
 while timer.time() < 5000:
     count += 1
-    sum += s.reflection()
+    sum += s1.reflection()
 ev3.screen.print(str(int(count / 5)))
 print("test2:"+str(int(count / 5)))
 
@@ -47,11 +47,48 @@ unique = 0
 prevValue = 0
 while timer.time() < 5000:
     count += 1
-    value = s.reflection()
+    value = s1.reflection()
     if value != prevValue:
         prevValue = value
         unique += 1
-ev3.screen.print(str(int(count / 5)) + " " + str(int(unique / 5)))
-print("test3: "+ str(int(count / 5)) + " " + str(int(unique / 5)))
+results = str(int(count / 5)) + " " + str(int(unique / 5))
+ev3.screen.print(results)
+print("test3: " + results)
+
+# test P-controller: read 4 color sensors and update power of two motors
+motorB = Motor(Port.B)
+motorC = Motor(Port.C)
+s2 = ColorSensor(Port.S2)
+s3 = ColorSensor(Port.S3)
+s4 = ColorSensor(Port.S4)
+timer.reset()
+count = 0
+uniqueB = 0
+uniqueC = 0
+prevValueB = 0
+prevValueC = 0
+while timer.time() < 5000:
+    count += 1
+    value1 = s1.reflection()
+    value2 = s2.reflection()
+    value3 = s3.reflection()
+    value4 = s4.reflection()
+    valueB = motorB.angle()
+    valueC = motorC.angle()
+    delta = (value1 * 2 + value2 - value3 - value4 * 2) / 6
+
+    motorB.dc(max(100 + delta, 100))
+    motorC.dc(max(100 - delta, 100))
+
+    if valueB != prevValueB:
+        prevValueB = valueB
+        uniqueB += 1
+    if valueC != prevValueC:
+        prevValueC = valueC
+        uniqueC += 1
+
+results = str(int(count / 5)) + " " + str(int(uniqueB / 5)) + " " + str(int(uniqueС / 5))
+ev3.screen.print(results)
+print("test3: " + results)
 
 wait(5000)
